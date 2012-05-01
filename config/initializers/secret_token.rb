f8a684d4e7613ec8c4ca3f89c107aefa2cc93f49
Zach Ewert <zachanima@gmail.com>
2012-05-01 15:11:53 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Evecalc::Application.config.secret_token = '04c4429c34e4f6ce7d594e362b665712c41dde5337a5853fe9fe3c98f66aa5279353dd4d4040a0bdc0a7d7414600462b40066ce34f9ed83bdeee8cdf9bd52e49'
