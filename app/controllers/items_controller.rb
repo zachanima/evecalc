ed4c0377f43850f95fc024c2c2eb8eb18b035d40
Zach Ewert <zachanima@gmail.com>
2012-05-02 00:46:37 +0200

    Add flash notice, redirect to new in item create

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new params[:item]
     if @item.save
-      redirect_to items_path
+      redirect_to new_item_path, notice: "Created #{@item.name}."
     else
       render action: :new
     end
