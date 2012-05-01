8ae39ee43e66c40360e4242a70ccdc7e5ca792d8
Zach Ewert <zachanima@gmail.com>
2012-05-01 19:06:49 +0200

    Add buy method to material model

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,4 +2,8 @@ class Material < ActiveRecord::Base
   attr_accessible :blueprint_id, :item_id, :quantity
   belongs_to :blueprint
   belongs_to :item
+
+  def buy
+    self.item.buy * self.quantity
+  end
 end
