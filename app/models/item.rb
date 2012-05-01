eab5ed010cd9fd47d79d4a088e17e6c23f6fec29
Zach Ewert <zachanima@gmail.com>
2012-05-02 00:06:31 +0200

    Remove comments from application js, css

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,6 @@ class Item < ActiveRecord::Base
   attr_accessible :buy, :sell, :name, :type_id
   has_many :materials
   has_many :blueprints, through: :materials
-  scope :outdated, where('updated_at < ? or buy is null or sell is null', Time.now - 6.hours).limit(100)
+  scope :outdated, where('updated_at < ? or buy is null or sell is null',
+                         Time.now - 6.hours).limit(100)
 end
