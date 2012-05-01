1317ff4a87990c53e8d14b24092f83432de2c802
Zach Ewert <zachanima@gmail.com>
2012-05-01 20:12:55 +0200

    Add items edit, create actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,8 +4,15 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new params[:item]
+    if @item.save
+      redirect_to items_path
+    else
+      render action: :new
+    end
   end
 end
